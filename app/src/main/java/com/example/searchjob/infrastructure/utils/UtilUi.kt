ee4761ea24c887a6b2c20cd95afdf42b94bf972a
dqq <lezniak.daniel@gmail.com>
2024-07-17 19:52:28 +0200

    login screen

--- app/src/main/java/com/example/searchjob/infrastructure/utils/UtilUi.kt
+++ app/src/main/java/com/example/searchjob/infrastructure/utils/UtilUi.kt
@@ -12,18 +12,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.searchjob.R
 
 @Composable
-fun HeaderAndText(@StringRes header: Int,@StringRes text: Int ,modifier: Modifier = Modifier) {
-    Column(modifier = modifier.fillMaxWidth().padding(16.dp), verticalArrangement = Arrangement.spacedBy(16.dp)) {
-        Text(text = stringResource(id = header), fontSize = 28.sp, fontWeight = Bold, lineHeight = 30.sp)
-        Text(text = stringResource(id = text), fontSize = 16.sp)
+fun HeaderAndText(@StringRes header: Int,@StringRes text: Int, isCenterded : Boolean = false ,modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxWidth().padding(16.dp), verticalArrangement = Arrangement.spacedBy(16.dp), horizontalAlignment = if(isCenterded) Alignment.CenterHorizontally else Alignment.Start) {
+        Text(text = stringResource(id = header), fontSize = 28.sp, fontWeight = Bold, lineHeight = 30.sp, textAlign = if (isCenterded) TextAlign.Center else TextAlign.Unspecified)
+        Text(text = stringResource(id = text), fontSize = 16.sp,textAlign = if (isCenterded) TextAlign.Center else TextAlign.Unspecified)
     }
 }
+
 
 @Preview
 @Composable
--- app/src/main/java/com/example/searchjob/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/searchjob/screens/login/LoginScreen.kt
@@ -1,13 +1,22 @@
 package com.example.searchjob.screens.login
 
+
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.searchjob.R
+import com.example.searchjob.infrastructure.utils.HeaderAndText
 
 @Composable
 fun LoginScreen() {
-    Text(text = "Login")
+    LoginSection()
+}
+
+@Composable
+private fun LoginSection(){
+    HeaderAndText(header = R.string.sign_up, text = R.string.enter_cred,true)
 }
 
 @Preview
